872a8b160baac515216c6594171b1037c1cb7d3a
Shiori Morita <afipito456@gmail.com>
2025-11-11 09:03:56 +0900

    メール認証機能の修正

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -5,8 +5,8 @@
 @section('content')
 <main class="verify-email">
     <p class="verify-email__message">登録していただいたメールアドレスに認証メールを送付しました。メール認証を完了してください。</p>
-    <a href="" class="verify-email__button">認証はこちらから</a>
-    <form action="" method="post">
+    <a href="http://localhost:8025" class="verify-email__button" target="_blank">認証はこちらから</a>
+    <form action="{{ route('verification.send') }}" method="post">
         @csrf
         <button type="submit" class="verify-email__link common-link">認証メールを再送する</button>
     </form>
