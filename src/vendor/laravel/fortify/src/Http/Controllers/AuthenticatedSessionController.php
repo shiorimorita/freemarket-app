44dcfda2653597a98dbeef85e22746c7c9845edc
Shiori Morita <afipito456@gmail.com>
2025-11-16 18:02:51 +0900

    修正

--- src/vendor/laravel/fortify/src/Http/Controllers/AuthenticatedSessionController.php
+++ src/vendor/laravel/fortify/src/Http/Controllers/AuthenticatedSessionController.php
@@ -16,7 +16,7 @@ use Laravel\Fortify\Contracts\LoginViewResponse;
 use Laravel\Fortify\Contracts\LogoutResponse;
 use Laravel\Fortify\Features;
 use Laravel\Fortify\Fortify;
-use Laravel\Fortify\Http\Requests\LoginRequest;
+use App\Http\Requests\LoginRequest;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -52,7 +52,7 @@ class AuthenticatedSessionController extends Controller
     /**
      * Attempt to authenticate a new session.
      *
-     * @param  \Laravel\Fortify\Http\Requests\LoginRequest  $request
+     * @param  \App\Http\Requests\LoginRequest  $request
      * @return mixed
      */
     public function store(LoginRequest $request)
--- src/vendor/laravel/fortify/src/Http/Controllers/RegisteredUserController.php
+++ src/vendor/laravel/fortify/src/Http/Controllers/RegisteredUserController.php
@@ -11,6 +11,7 @@ use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Fortify\Contracts\RegisterResponse;
 use Laravel\Fortify\Contracts\RegisterViewResponse;
 use Laravel\Fortify\Fortify;
+use App\Http\Requests\RegisterRequest;
 
 class RegisteredUserController extends Controller
 {
@@ -50,9 +51,10 @@ class RegisteredUserController extends Controller
      * @param  \Laravel\Fortify\Contracts\CreatesNewUsers  $creator
      * @return \Laravel\Fortify\Contracts\RegisterResponse
      */
-    public function store(Request $request,
-                          CreatesNewUsers $creator): RegisterResponse
-    {
+    public function store(
+        RegisterRequest $request,
+        CreatesNewUsers $creator
+    ): RegisterResponse {
         if (config('fortify.lowercase_usernames')) {
             $request->merge([
                 Fortify::username() => Str::lower($request->{Fortify::username()}),
